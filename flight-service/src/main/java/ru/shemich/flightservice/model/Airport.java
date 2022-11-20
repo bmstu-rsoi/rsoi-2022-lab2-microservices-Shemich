4e13d228dcff828b97bbefebc8bae5cdccc28bda
shemich <bazzzalex@mail.ru>
2022-11-20 03:10:43 +0300

    chore: add airport entity

---
+++ flight-service/src/main/java/ru/shemich/flightservice/model/Airport.java
@@ -0,0 +1,31 @@
+package ru.shemich.flightservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PRIVATE;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
+@Table(name = "airport")
+public class Airport {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
+    Integer id;
+    @Column(name = "name")
+    String name;
+    @Column(name = "city")
+    String city;
+    @Column(name = "country")
+    String country;
+}
+
---
+++ flight-service/src/main/java/ru/shemich/flightservice/repository/AirportRepository.java
@@ -0,0 +1,9 @@
+package ru.shemich.flightservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.shemich.flightservice.model.Airport;
+
+@Repository
+public interface AirportRepository extends JpaRepository<Airport, Integer> {
+}
