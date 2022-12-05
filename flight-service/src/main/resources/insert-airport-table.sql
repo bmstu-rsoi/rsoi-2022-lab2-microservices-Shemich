648be1472020b64a3a9c7abb6dc5fd1e21a2e721
shemich <bazzzalex@mail.ru>
2022-12-06 02:38:49 +0300

    fix(docker): url and data

--- flight-service/src/main/resources/insert-airport-table.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO AIRPORT (ID, NAME, CITY, COUNTRY)
-VALUES (1, 'Санкт-Петербург Пулково', 'Санкт-Петербург', 'Россия');
-INSERT INTO AIRPORT (ID, NAME, CITY, COUNTRY)
-VALUES (2, 'Москва Шереметьево', 'Москва', 'Россия');
---
+++ postgres/40-insert-airport-table.sql
@@ -0,0 +1,2 @@
+INSERT INTO AIRPORT (ID, NAME, CITY, COUNTRY) VALUES (1, 'Пулково', 'Санкт-Петербург', 'Россия');
+INSERT INTO AIRPORT (ID, NAME, CITY, COUNTRY) VALUES (2, 'Шереметьево', 'Москва', 'Россия');
