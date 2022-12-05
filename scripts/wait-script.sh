b80bf4c93337257ebeb817096e693f2e909fef52
shemich <bazzzalex@mail.ru>
2022-12-06 02:45:24 +0300

    fix(postman): add delay

--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -2,6 +2,10 @@
 
 IFS="," read -ra PORTS <<<"$WAIT_PORTS"
 
+echo "Sleeping for 30 seconds"
+sleep 30
+echo "Wake up Neo. Sorry copied)"
+
 PIDs=()
 for port in "${PORTS[@]}"; do
   "$(pwd)"/scripts/wait-for.sh -t 120 "localhost:$port" -- echo "Host localhost:$port is active" &
